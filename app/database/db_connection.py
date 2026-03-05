9ed6b385cbcb9a8ebaa2ea2e7d9d4dfcaf5349bd
Sriram Nitheesh <135050456+Nitheesh14125@users.noreply.github.com>
2026-03-05 19:29:57 +0530

    Update db_connection.py

--- app/database/db_connection.py
+++ app/database/db_connection.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://localhost/resume_analyzer"
+DATABASE_URL = "mysql+pymysql://localhost/resume_analyzer" #enter you db user and db password
 
 engine = create_engine(DATABASE_URL)
 
